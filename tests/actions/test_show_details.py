8024d1ac0e29de8d21064352dfe85ae14d7b38f5
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-07-05 17:25:31 -0300

    refact: req 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,16 +1,46 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
-import pytest
-from unittest.mock import patch, Mock
+import pytest # NOQA
+from unittest.mock import patch, Mock # NOQA
 
 
 class TestShowDetails:
+    def test_file_not_found(self, capsys):
+        mock_os_path_exists = Mock(return_value=False)
+        fake_file_context = {"base_path": "/home/trybe/????"}
+
+        with patch("os.path.exists", mock_os_path_exists):
+            show_details(fake_file_context)
+            captured = capsys.readouterr()
+            assert captured.out == "File '????' does not exist\n"
+
+    @pytest.fixture
+    def mock_os_path_exists(self):
+        with patch("os.path.exists") as mock:
+            yield mock
+
+    @pytest.fixture
+    def mock_os_path_getsize(self):
+        with patch("os.path.getsize") as mock:
+            yield mock
+
+    @pytest.fixture
+    def mock_os_path_isdir(self):
+        with patch("os.path.isdir") as mock:
+            yield mock
+
+    @pytest.fixture
+    def mock_os_path_splitext(self):
+        with patch("os.path.splitext") as mock:
+            yield mock
+
+    @pytest.fixture
+    def mock_os_path_getmtime(self):
+        with patch("os.path.getmtime") as mock:
+            yield mock
+
     @pytest.mark.parametrize(
         "context, expected_output",
         [
-            (
-                {"base_path": "/home/trybe/????"},
-                "File '????' does not exist\n"
-            ),
             (
                 {"base_path": "/home/trybe/Downloads/Trybe_logo.png"},
                 [
@@ -21,6 +51,27 @@ class TestShowDetails:
                     "Last modified date: 2019-05-21\n",
                 ],
             ),
+        ],
+    )
+    def test_file_found(
+        self, capsys, context, expected_output, mock_os_path_exists,
+        mock_os_path_getsize, mock_os_path_isdir, mock_os_path_splitext,
+        mock_os_path_getmtime
+    ):
+        mock_os_path_exists.return_value = True
+        mock_os_path_getsize.return_value = 100
+        mock_os_path_isdir.return_value = False
+        mock_os_path_splitext.return_value = ("Trybe_logo", ".png")
+        mock_os_path_getmtime.return_value = 1558447897.0442736
+
+        show_details(context)
+        captured = capsys.readouterr()
+        for output in expected_output:
+            assert output in captured.out
+
+    @pytest.mark.parametrize(
+        "context, expected_output",
+        [
             (
                 {"base_path": "/home/trybe/Downloads"},
                 [
@@ -33,33 +84,18 @@ class TestShowDetails:
             ),
         ],
     )
-    def test_show_details(
-        self, capsys, context, expected_output
+    def test_directory_found(
+        self, capsys, context, expected_output, mock_os_path_exists,
+        mock_os_path_getsize, mock_os_path_isdir, mock_os_path_splitext,
+        mock_os_path_getmtime
     ):
-        mock_os_path_exists = Mock(
-            return_value="????" not in context["base_path"])
-        mock_os_path_getsize = Mock(
-            return_value=500 if expected_output[0].startswith("File name")
-            else 100)
-        mock_os_path_isdir = Mock(
-            return_value="directory" in expected_output[2])
-        mock_os_path_splitext = Mock(
-            return_value=("", ".png")
-            if ".png" in expected_output[0] else ("", ""))
-        mock_os_path_getmtime = Mock(
-            return_value=1558447897.0442736)
+        mock_os_path_exists.return_value = True
+        mock_os_path_getsize.return_value = 500
+        mock_os_path_isdir.return_value = True
+        mock_os_path_splitext.return_value = ("Downloads", "")
+        mock_os_path_getmtime.return_value = 1558447897.0442736
 
-        with patch("os.path.exists", mock_os_path_exists), \
-             patch("os.path.getsize", mock_os_path_getsize), \
-             patch("os.path.isdir", mock_os_path_isdir), \
-             patch("os.path.splitext", mock_os_path_splitext), \
-             patch("os.path.getmtime", mock_os_path_getmtime):
-
-            show_details(context)
-            captured = capsys.readouterr()
-
-            if isinstance(expected_output, str):
-                assert captured.out == expected_output
-            else:
-                for output in expected_output:
-                    assert output in captured.out
+        show_details(context)
+        captured = capsys.readouterr()
+        for output in expected_output:
+            assert output in captured.out
