14685292239fe8159f5e467d2c4da3b49e6db3c8
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-07-05 17:19:08 -0300

    req 5

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,36 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+from pro_filer.cli_helpers import _get_printable_file_path  # NOQA
+from typing import Dict, Any # NOQA
+
+
+empty_context: Dict[str, Any] = {
+    "all_files": []
+}
+
+
+def test_show_disk_usage(capsys, tmp_path):
+    file = tmp_path / "test.py"
+    file.touch()
+    file.write_text("test")
+    path = str(file)
+
+    empty_file = tmp_path / "empty.py"
+    empty_file.touch()
+    path_empty = str(empty_file)
+
+    context = {
+        "all_files": [path, path_empty],
+    }
+
+    show_disk_usage(context)
+    result = capsys.readouterr()
+    output = f"'{_get_printable_file_path(path)}':".ljust(70)
+    empty_output = f"'{_get_printable_file_path(path_empty)}':".ljust(70)
+    assert result.out == f"{output} 4 (100%)\n\
+{empty_output} 0 (0%)\nTotal size: 4\n"
+
+
+def test_show_disk_usage_no_files(capsys):
+    show_disk_usage(empty_context)
+    result = capsys.readouterr()
+    assert result.out == "Total size: 0\n"
