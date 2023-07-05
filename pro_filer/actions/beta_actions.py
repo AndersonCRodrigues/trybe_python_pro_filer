df161488b0e3cfcb4279adfa2c979b7607f32563
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-07-05 17:18:34 -0300

    req 1

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,10 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"],
+            key=lambda file: file.count('/')
+            )
         print(f"Deepest file: {deepest_file}")
 
 
@@ -19,8 +22,8 @@ def find_file_by_name(context, search_term, case_sensitive=True):
         file_name = path.split("/")[-1]
 
         if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+            file_name = file_name.lower()
+            search_term = search_term.lower()
 
         if search_term in file_name:
             found_files.append(path)
